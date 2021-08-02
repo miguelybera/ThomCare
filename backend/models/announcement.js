89d6047cfa7f786243a40c3a225bedf829dc6dff
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-08-02 18:04:27 +0800

    cron job

--- backend/models/announcement.js
+++ backend/models/announcement.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const cron = require("node-cron");
 const announcementSchema = new mongoose.Schema({
     
         title: {
@@ -17,7 +18,27 @@ const announcementSchema = new mongoose.Schema({
         },
         archiveDate:{
             type: Date
+        },
+        isArchive:{
+            type: Boolean,
+            default: false
         }
 })
 
+announcementSchema.post("save", function(announcement, next ){
+    cron.schedule('* * * * *', 
+    async function() { 
+        const dateToday = Date.now();
+        if(announcement.archiveDate.valueOf() <= dateToday.valueOf() ){
+            const result = await mongoose.model("Announcement")
+            .update({ "_id" : announcement._id}, {$set: {isArchive:true}});
+
+            if(result.nModified===1){
+                console.log("isArchive set to true for", announcement.title);
+            }
+        }     
+}
+);
+    next ();
+});
 module.exports = mongoose.model('Announcement', announcementSchema);
