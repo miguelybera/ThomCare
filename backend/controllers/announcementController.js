8243c49805651527163d13104cea9d1e08b01e63
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-08-10 18:07:43 +0800

    push again for announcement

--- backend/controllers/announcementController.js
+++ backend/controllers/announcementController.js
@@ -65,8 +65,6 @@ exports.newAnnouncement = catchAsyncErrors (async (req,res,next)=>{
             return next(new ErrorHandler('Course does not match this track', 400))
         } 
     }
-        
-    
     const announcement = await Announcement.create({
         title, 
         description, 
@@ -241,6 +239,24 @@ exports.updateAnnouncement = catchAsyncErrors (async(req,res,next) =>{
                 return next(new ErrorHandler('Course does not match this track', 400))
             } 
         }
+    }
+    if(course == "Computer Science"){
+        if(track == "Core Computer Science" || track == "Game Development" || track == "Data Science" || track == "All"){
+        }else{
+            return next(new ErrorHandler('Course does not match this track', 400))
+        } 
+    }
+    if(course == "Information Technology"){
+        if(track == "Network and Security" || track == "Web and Mobile App Development" || track == "IT Automation" || track == "All"){
+        }else{
+            return next(new ErrorHandler('Course does not match this track', 400))
+        } 
+    }
+    if(course === "Information Systems"){
+        if(track == "Business Analytics" || track == "Service Management" || track == "All"){
+        }else{
+            return next(new ErrorHandler('Course does not match this track', 400))
+        } 
     }
     let newAnnouncementData = {
         title: newTitle,
