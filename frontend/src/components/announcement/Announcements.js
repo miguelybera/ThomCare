384fcef66cebf3b30e55699aad2d0c09408cc5b0
fromabby <60252891+fromabby@users.noreply.github.com>
2021-08-18 23:16:51 +0800

    updated announcement filters

--- frontend/src/components/announcement/Announcements.js
+++ frontend/src/components/announcement/Announcements.js
@@ -6,9 +6,32 @@ import Pagination from 'react-js-pagination'
 import { getAnnouncements, clearErrors } from './../../actions/announcementActions'
 import MetaData from './../layout/MetaData'
 import Loader from './../layout/Loader'
-import { Accordion, ButtonGroup, Button, ButtonToolbar, DropdownButton, Dropdown, Form, FormControl, Card, Row, Col } from 'react-bootstrap'
+import { Accordion, ButtonGroup, Button, ButtonToolbar, DropdownButton, Dropdown, Form, FormControl, Card, Row, Col, Container } from 'react-bootstrap'
 
 var dateFormat = require('dateformat')
+
+const titles = {
+    width: '10px',
+    color: 'white',
+    paddingTop: '160px',
+    paddingBottom: '10px'
+}
+
+const rectangle = {
+    width:'100%',
+    height:'18%',                   /* gran changed this from 22 > 18 */
+    background: '#9c0b0b',
+    textAlign: 'center',
+    color: 'white',
+    position:'absolute',
+    fontFamily: 'Mukta Malar',
+    fontWeight: '500',
+    paddingTop: '100px',
+    paddingRight: '50px',
+    zIndex: -1,
+    top: 0,
+    left: 0
+}
 
 const Announcements = () => {
 
@@ -79,67 +102,82 @@ const Announcements = () => {
     return (
         <>
             <MetaData title={`Announcements`} />
-            <h1> Announcements </h1>
-            <Form>
-                <Row>
-                    <Form.Group as={Col} xs={2} controlId="selectCourseforAnnouncement">
-                        <Form.Label>Course</Form.Label>
-                        <Form.Select aria-label="Course" size="sm" name="course" value={course} onChange={onChange} required>
-                            <option value=''>-</option>
-                            <option value="Computer Science">Computer Science</option>
-                            <option value="Information Systems">Information Systems</option>
-                            <option value="Information Technology">Information Technology</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} xs={2} controlId="selectYearLevelforAnnouncement">
-                        <Form.Label>Year Level</Form.Label>
-                        <Form.Select aria-label="YearLevel" size="sm" name="yearLevel" value={yearLevel} onChange={onChange} required>
-                            <option value=''>-</option>
-                            <option value="1st Year">1st Year</option>
-                            <option value="2nd Year">2nd Year</option>
-                            <option value="3rd Year">3rd Year</option>
-                            <option value="4th Year">4th Year</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} xs={3} controlId="selectTrackforAnnouncement">
-                        <Form.Label>Track</Form.Label>
-                        <Form.Select aria-label="ITTracks" size="sm" name="track" value={track} onChange={onChange} required>
-                            <option value=''>-</option>
-                            <option value="Web and Mobile Development">Web and Mobile Development</option>
-                            <option value="Network and Security">Network and Security</option>
-                            <option value="IT Automation">IT Automation</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} xs={2} controlId="selectAnnouncementTypeforAnnouncement">
-                        <Form.Label>Announcement Type</Form.Label>
-                        <Form.Select aria-label="AnnouncementType" size="sm" name="annnouncementType" value={annnouncementType} onChange={onChange} required>
-                            <option value=''>-</option>
-                            <option value="Memorandum">Memorandum</option>
-                            <option value="Enrollment">Enrollment</option>
-                            <option value="Class Suspension">Class Suspension</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="seachAnnouncement" xs={2}>
-                        <Form.Label>Search by title</Form.Label>
-                        <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="mr-2"
-                            aria-label="Search"
-                            size="sm"
-                            name="title"
-                            value={title}
-                            onChange={onChange}
-                        />
-                    </Form.Group>
-                </Row>
-            </Form>
-
+            <Container style={titles}>
+                <div style={rectangle}>
+                    <h3>Announcements</h3>
+                </div>
+            </Container>
+            <Container>
+                <Form>
+                    <Row xs="auto">
+                        <Col sm>
+                            <Form.Group controlId="selectYearLevelforAnnouncement">
+                                <Form.Label>Year Level</Form.Label>
+                                <Form.Select aria-label="YearLevel" size="sm" name="yearLevel" value={yearLevel} onChange={onChange} required>
+                                    <option value=''>-</option>
+                                    <option value="1st Year">1st Year</option>
+                                    <option value="2nd Year">2nd Year</option>
+                                    <option value="3rd Year">3rd Year</option>
+                                    <option value="4th Year">4th Year</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                        <Col sm>
+                            <Form.Group controlId="selectCourseforAnnouncement">
+                                <Form.Label>Course</Form.Label>
+                                <Form.Select aria-label="Course" size="s" name="course" value={course} onChange={onChange} required>
+                                    <option value=''></option>
+                                    <option value="Computer Science">Computer Science</option>
+                                    <option value="Information Systems">Information Systems</option>
+                                    <option value="Information Technology">Information Technology</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                        <Col sm>
+                            <Form.Group controlId="selectTrackforAnnouncement">
+                                <Form.Label>Track</Form.Label>
+                                <Form.Select aria-label="ITTracks" size="sm" name="track" value={track} onChange={onChange} required>
+                                    <option value=''>-</option>
+                                    <option value="Web and Mobile Development">Web and Mobile Development</option>
+                                    <option value="Network and Security">Network and Security</option>
+                                    <option value="IT Automation">IT Automation</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                        <Col sm>
+                            <Form.Group controlId="selectAnnouncementTypeforAnnouncement">
+                                <Form.Label>Announcement Type</Form.Label>
+                                <Form.Select aria-label="AnnouncementType" size="sm" name="annnouncementType" value={annnouncementType} onChange={onChange} required>
+                                    <option value=''>-</option>
+                                    <option value="Memorandum">Memorandum</option>
+                                    <option value="Enrollment">Enrollment</option>
+                                    <option value="Class Suspension">Class Suspension</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                        <Col sm>
+                            <Form.Group controlId="seachAnnouncement" xs={2}>
+                                <Form.Label>Search by title</Form.Label>
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="mr-2"
+                                    aria-label="Search"
+                                    size="sm"
+                                    name="title"
+                                    value={title}
+                                    onChange={onChange}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
             <Row>
                 {loading ? <Loader /> : (
                     <Row xs={1} md={2} className="g-4">
                         {announcements && (announcements.length !== 0) ? announcements.map(announcement => (
-                            <Col>
+                            <Col style={{marginBottom: '20px'}}>
                                 <Card>
                                     <Card.Body>
                                         <Card.Title>{announcement.title}</Card.Title>
