1cb640d111ddcf5d6a4dd5ec6dbe24984afc0f63
fromabby <60252891+fromabby@users.noreply.github.com>
2021-08-29 01:09:04 +0800

    added new fields in sample form

--- frontend/src/components/request/SampleForm.js
+++ frontend/src/components/request/SampleForm.js
@@ -1,16 +1,31 @@
 import React, { Fragment, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { getCourses, clearErrors } from './../../actions/courseActions'
+import MetaData from './../layout/MetaData'
 import { Table, Row, InputGroup, FormControl, Container, Button, Col, Card, Form, ListGroup } from 'react-bootstrap'
-import AddDrop from './AddDrop'
 
 function SampleForm() {
+    const alert = useAlert()
+    const dispatch = useDispatch()
+
+    const { courses, success, error, loading } = useSelector(state => state.courses)
+    const { user } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        dispatch(getCourses())
+
+        if (error) {
+            alert.error(error)
+            dispatch(clearErrors())
+        }
+
+    }, [dispatch, alert, error])
 
     //new 
     const [inputFields, setInputFields] = useState([
         {
-            index: Math.random(),
             status: '',
             courseCode: '',
             courseName: '',
@@ -25,21 +40,34 @@ function SampleForm() {
     const onChange = (index, e) => {
         e.preventDefault()
         const values = [...inputFields]
-        
+
         values[index][e.target.name] = e.target.value
 
+        if (values[index]["courseCode"] !== '') {
+            values[index]["courseName"] = getCourseName(values[index]["courseCode"], "courseName")
+            values[index]["labUnits"] = getCourseName(values[index]["courseCode"], "labUnits")
+            values[index]["lecUnits"] = getCourseName(values[index]["courseCode"], "lecUnits")
+        }
         setInputFields(values)
     }
 
     const submitHandler = e => {
         e.preventDefault()
 
-        localStorage.setItem('formData', JSON.stringify(inputFields))
+        const formData = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            middleName: user.middleName ? user.middleName : 'N/A',
+            studentNumber: user.studentNumber,
+            email: user.email,
+            course: user.course,
+            addDrop: inputFields
+        }
+        localStorage.setItem('formData', JSON.stringify(formData))
     }
 
     const addRow = () => {
         setInputFields([...inputFields, {
-            index: Math.random(),
             status: '',
             courseCode: '',
             courseName: '',
@@ -59,60 +87,61 @@ function SampleForm() {
         setInputFields(values)
     }
 
+    const getCourseName = (code, field) => {
+        const val = courses.find(course => course.courseCode === code)
+
+        switch (field) {
+            case "courseName":
+                return val.courseName
+            case "lecUnits":
+                return val.lecUnits
+            case "labUnits":
+                return val.labUnits
+        }
+    }
+
     return (
         <Fragment>
+            <MetaData title={'Add/Drop Form'} />
             <Container classname="align-me" fluid style={{ paddingBottom: '100px' }}>
                 <Card style={{ backgroundColor: '#9c0b0b' }}>  {/*, width: '100rem' */}
                     <Card.Body>
                         <Card.Title style={{ margin: '10px 0 20px 0', color: 'white', fontWeight: 'bold' }}>Student Information</Card.Title>
                         <Form style={{ color: 'white' }} onSubmit={submitHandler} >
-                            {/**
-                             * <Row className="mb-3">
+                            <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>First Name</Form.Label>
-                                    <Form.Control type="text" placeholder="Gran Austhyn" readOnly />
+                                    <Form.Control type="text" value={user && user.firstName} readOnly />
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridEmail">
                                     <Form.Label>Last Name</Form.Label>
-                                    <Form.Control type="text" placeholder="De Vera" readOnly />
+                                    <Form.Control type="text" value={user && user.lastName} readOnly />
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Label>Middle Name</Form.Label>
-                                    <Form.Control type="text" placeholder="S." readOnly />
+                                    <Form.Label>Middle Initial</Form.Label>
+                                    <Form.Control type="text" placeholder="S." value={user && user.middleName ? user.middleName : 'N/A'} readOnly />
                                 </Form.Group>
                             </Row>
 
                             <Form.Group className="mb-3" controlId="formGridAddress1">
                                 <Form.Label>Student Number</Form.Label>
-                                <Form.Control placeholder="2018116830" readOnly />
+                                <Form.Control value={user && user.studentNumber} readOnly />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formGridAddress2">
                                 <Form.Label>Course/Program</Form.Label>
-                                <Form.Control type="text" placeholder="Information Technology" readOnly />
+                                <Form.Control type="text" value={user && user.course} readOnly />
                             </Form.Group>
-                             
 
                             <Row className="mb-3">
                                 <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type='email' placeholder="granausthyn.devera.iics@ust.edu.ph" readOnly />
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridAddress2">
-                                    <Form.Label>Category of Courses</Form.Label>
-                                    <Form.Select aria-label="Default select example">
-                                        <option>-</option>
-                                        <option value="GE">General Education(GE)</option>
-                                        <option value="NSTP">National Service Training Program (NSTP)</option>
-                                        <option value="PE">Physical Education (PE)</option>
-                                        <option value="P">Professional</option>
-                                    </Form.Select>
+                                    <Form.Control type='email' value={user && user.email} readOnly />
                                 </Form.Group>
                             </Row>
-                            */}
+
                             <Card.Title style={{ margin: '10px 0 20px 0', color: 'white', fontWeight: 'bold' }}>Courses to Add / Drop</Card.Title>
 
                             <Row className="mb-3">
@@ -165,7 +194,7 @@ function SampleForm() {
                                         <Fragment>
                                             <Row className="mb-3" key={val.index}>
                                                 <Form.Group as={Col}>
-                                                    <Form.Select aria-label="Default select example" name="status" id={status} data-id={idx} value={val.status} onChange={e => onChange(idx, e)}>
+                                                    <Form.Select aria-label="Default select example" name="status" id={status} data-id={idx} value={val.status} onChange={e => onChange(idx, e)} required>
                                                         <option>-</option>
                                                         <option value="Add">Add</option>
                                                         <option value="Drop">Drop</option>
@@ -173,31 +202,36 @@ function SampleForm() {
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="text" placeholder="ICS2021/IT2021" name="courseCode" id={courseCode} data-id={idx} value={val.courseCode} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Select aria-label="Default select example" name="courseCode" id={courseCode} data-id={idx} value={val.courseCode} onChange={e => onChange(idx, e)} required>
+                                                        <option>-</option>
+                                                        {courses && courses.map(course => (
+                                                            <option value={course.courseCode}>{course.courseCode}</option>
+                                                        ))}
+                                                    </Form.Select>
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="text" placeholder="Theology 1" name="courseName" id={courseName} data-id={idx} value={val.courseName} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="text" placeholder="Theology 1" name="courseName" id={courseName} data-id={idx} value={val.courseName} onChange={e => onChange(idx, e)} readOnly />
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="number" placeholder="3Lec" name="lecUnits" id={lecUnits} data-id={idx} value={val.lecUnits} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="number" placeholder="3Lec" name="lecUnits" id={lecUnits} data-id={idx} value={val.lecUnits} onChange={e => onChange(idx, e)} readOnly />
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="number" placeholder="3Lec" name="labUnits" id={labUnits} data-id={idx} value={val.labUnits} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="number" placeholder="3Lec" name="labUnits" id={labUnits} data-id={idx} value={val.labUnits} onChange={e => onChange(idx, e)} readOnly />
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="number" placeholder="14 Days" name="days" id={days} data-id={idx} value={val.days} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="number" placeholder="14 Days" name="days" id={days} data-id={idx} value={val.days} onChange={e => onChange(idx, e)} required />
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="text" placeholder="3:00PM - 5:00PM" name="time" id={time} data-id={idx} value={val.time} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="text" placeholder="3:00PM - 5:00PM" name="time" id={time} data-id={idx} value={val.time} onChange={e => onChange(idx, e)} required />
                                                 </Form.Group>
 
                                                 <Form.Group as={Col}>
-                                                    <Form.Control type="text" placeholder="4ITF" name="section" id={section} data-id={idx} value={val.section} onChange={e => onChange(idx, e)}/>
+                                                    <Form.Control type="text" placeholder="4ITF" name="section" id={section} data-id={idx} value={val.section} onChange={e => onChange(idx, e)} required />
                                                 </Form.Group>
 
                                                 {
