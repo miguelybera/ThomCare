66e824abc1ea357ba93731fc0539a05ba8c86175
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-09-16 02:17:17 +0800

    update report cards

--- frontend/src/components/dashboard/controlpanel/ReportCard.js
+++ frontend/src/components/dashboard/controlpanel/ReportCard.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react'
 import './styles.css'
 
-
 const ReportCard = ({ requestType, length, icon }) => {
     return (<Fragment >
         <div class="card bg-c-blue order-card">
                 <div class="card-block">
                     <h6 class="m-b-20">{requestType}</h6>
-                    <h2 class="text-right"><i className={{icon}}></i><span>{length}</span></h2>
+                    <h2 class="text-right"><i class="fa fa-cart-plus f-right"></i><span>{length}</span></h2>
                     <p class="m-b-0"> Total {requestType}</p>
                 </div>
             </div>
