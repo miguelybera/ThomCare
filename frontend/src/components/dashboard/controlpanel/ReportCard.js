2a0cadd2de7a8c39d8172c7451c360dbcc9174a7
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-09-16 02:05:00 +0800

    update to cards control panel

--- frontend/src/components/dashboard/controlpanel/ReportCard.js
+++ frontend/src/components/dashboard/controlpanel/ReportCard.js
@@ -1,23 +1,16 @@
 import React, { Fragment } from 'react'
+import './styles.css'
+
 
 const ReportCard = ({ requestType, length, icon }) => {
     return (<Fragment >
-        <div className="col-xl-3 col-sm-6 col-12" >
-            <div className="card" >
-                <div className="card-content" >
-                    <div className="card-body" >
-                        <div className="media d-flex" >
-                            <div className="align-self-center" >
-                                <i className={`icon-${icon} primary font-large-2 float-left`} > </i> </div>
-                            <div className="media-body text-right" >
-                                <h3 > {length} </h3>
-                                <span > Total {requestType} </span>
-                            </div>
-                        </div>
-                    </div>
+        <div class="card bg-c-blue order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">{requestType}</h6>
+                    <h2 class="text-right"><i className={{icon}}></i><span>{length}</span></h2>
+                    <p class="m-b-0"> Total {requestType}</p>
                 </div>
             </div>
-        </div>
     </Fragment>
     )
 }
