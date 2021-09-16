e15bcce3ddb4adfdb6dfb517e38024e15941656d
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-09-16 10:36:19 +0800

    color report card red

--- frontend/src/components/dashboard/controlpanel/ReportCard.js
+++ frontend/src/components/dashboard/controlpanel/ReportCard.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const ReportCard = ({ requestType, length, icon }) => {
     return (<Fragment >
-        <div class="card bg-c-green order-card">
+        <div class="card bg-c-red order-card">
                 <div class="card-block">
                     <h6 class="m-b-20">{requestType}</h6>
                     <h2 class="text-right"><i class={icon}></i><span> {length}</span></h2>
