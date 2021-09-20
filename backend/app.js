4c082c1167aaf7864ba310e17fb2232f2b9c1d4e
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-09-20 10:16:32 +0800

    asdasd

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const errorMiddleware = require('./middlewares/errors');
 
 // Setting up config file
-if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+//if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,13 +32,14 @@ app.use('/api/v1', announcementType)
 app.use('/api/v1', course)
 app.use('/api/v1', form)
 
+/*
 if(process.env.NODE_ENV === 'PRODUCTION'){
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
-
+*/
 // Middleware to handle errors
 app.use(errorMiddleware);
 
