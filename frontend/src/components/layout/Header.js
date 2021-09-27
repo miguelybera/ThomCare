8ccd87c01675680f58094006bcc3833b7e60cb26
fromabby <60252891+fromabby@users.noreply.github.com>
2021-09-27 20:36:23 +0800

    tried mern stack video

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -42,10 +42,11 @@ const Header = ({ history }) => {
     const dispatch = useDispatch()
     const alert = useAlert()
 
-    const { user } = useSelector(state => state.auth)
+    const { loading, user } = useSelector(state => state.auth)
 
     const logoutHandler = () => {
         dispatch(logout())
+        alert.success('Logged out successfully.')
     }
 
     return (
@@ -86,70 +87,72 @@ const Header = ({ history }) => {
                                 </h1>
                             </Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            {user ? (
-                                <>
-                                    <Navbar.Collapse id="responsive-navbar-nav"
-                                        style={{
-                                            float: "right",
-                                            fontSize: "85%",
-                                            fontWeight: "bold",
-                                        }}>
-                                        <Nav variant="pills"
-                                            className="image"
-                                        >
-                                            <Nav.Link className="image"><Link to='/' activeStyle>Announcements</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/forms/list' activeStyle>Generate Form</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/download/forms/list' activeStyle>Downloadable Forms</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/submit/request' activeStyle onClick={() => {
-                                                dispatch({
-                                                    type: SUBMIT_REQUEST_RESET
-                                                })
-                                            }}>Submit Request</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/track' activeStyle>Track my Request</Link></Nav.Link>
-                                        </Nav>
-                                        <Drop>
-                                            <NavDropdown title={`${user.firstName}`} id="basic-nav-dropdown">
-                                                <NavDropdown.Item><Link to='/controlpanel'>Control Panel</Link></NavDropdown.Item>
-                                                <NavDropdown.Item><Link to='/profile'>My Profile</Link></NavDropdown.Item>
-                                                <NavDropdown.Item><Link to='/messenger'>Messenger</Link></NavDropdown.Item>
-                                                <NavDropdown.Divider />
-                                                <NavDropdown.Item style={{ color: 'red' }}>
-                                                    <span onClick={() => logoutHandler()}>
-                                                        Log out
-                                                    </span>
-                                                </NavDropdown.Item>
-                                            </NavDropdown>
-                                        </Drop>
-                                    </Navbar.Collapse>
-                                </>) : (
-                                <>
-                                    <Navbar.Collapse id="responsive-navbar-nav"
-                                        style={{
-                                            float: "right",
-                                            fontSize: "85%",
-                                            fontWeight: "bold",
-                                        }}>
-                                        <Nav variant="pills"
-                                            className="image"
+                            {loading ? <Fragment></Fragment> : (
+                                user ? (
+                                    <>
+                                        <Navbar.Collapse id="responsive-navbar-nav"
                                             style={{
-                                                marginLeft: "auto",
-                                                paddingRight: "15px",
-                                                paddingLeft: "15px"
-
+                                                float: "right",
+                                                fontSize: "85%",
+                                                fontWeight: "bold",
                                             }}>
-                                            <Nav.Link className="image"><Link to='/' activeStyle>Announcements</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/forms/list' activeStyle>Generate Form</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/download/forms/list' activeStyle>Downloadable Forms</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/submit/request' activeStyle onClick={() => {
-                                                dispatch({
-                                                    type: SUBMIT_REQUEST_RESET
-                                                })
-                                            }}>Submit Request</Link></Nav.Link>
-                                            <Nav.Link className="image"><Link to='/track' activeStyle>Track my Request</Link></Nav.Link>
-                                            <Nav.Link className="image" href="/login">Login</Nav.Link>
-                                        </Nav>
-                                    </Navbar.Collapse>
-                                </>
+                                            <Nav variant="pills"
+                                                className="image"
+                                            >
+                                                <Nav.Link className="image"><Link to='/' activeStyle>Announcements</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/forms/list' activeStyle>Generate Form</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/download/forms/list' activeStyle>Downloadable Forms</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/submit/request' activeStyle onClick={() => {
+                                                    dispatch({
+                                                        type: SUBMIT_REQUEST_RESET
+                                                    })
+                                                }}>Submit Request</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/track' activeStyle>Track my Request</Link></Nav.Link>
+                                            </Nav>
+                                            <Drop>
+                                                <NavDropdown title={`${user.firstName}`} id="basic-nav-dropdown">
+                                                    <NavDropdown.Item><Link to='/controlpanel'>Control Panel</Link></NavDropdown.Item>
+                                                    <NavDropdown.Item><Link to='/profile'>My Profile</Link></NavDropdown.Item>
+                                                    <NavDropdown.Item><Link to='/messenger'>Messenger</Link></NavDropdown.Item>
+                                                    <NavDropdown.Divider />
+                                                    <NavDropdown.Item style={{ color: 'red' }}>
+                                                        <Link to='/' onClick={logoutHandler}>
+                                                            Log out
+                                                        </Link>
+                                                    </NavDropdown.Item>
+                                                </NavDropdown>
+                                            </Drop>
+                                        </Navbar.Collapse>
+                                    </>) : (
+                                    <>
+                                        <Navbar.Collapse id="responsive-navbar-nav"
+                                            style={{
+                                                float: "right",
+                                                fontSize: "85%",
+                                                fontWeight: "bold",
+                                            }}>
+                                            <Nav variant="pills"
+                                                className="image"
+                                                style={{
+                                                    marginLeft: "auto",
+                                                    paddingRight: "15px",
+                                                    paddingLeft: "15px"
+
+                                                }}>
+                                                <Nav.Link className="image"><Link to='/' activeStyle>Announcements</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/forms/list' activeStyle>Generate Form</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/download/forms/list' activeStyle>Downloadable Forms</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/submit/request' activeStyle onClick={() => {
+                                                    dispatch({
+                                                        type: SUBMIT_REQUEST_RESET
+                                                    })
+                                                }}>Submit Request</Link></Nav.Link>
+                                                <Nav.Link className="image"><Link to='/track' activeStyle>Track my Request</Link></Nav.Link>
+                                                <Nav.Link className="image" href="/login">Login</Nav.Link>
+                                            </Nav>
+                                        </Navbar.Collapse>
+                                    </>
+                                )
                             )}
                         </Container>
                     </Navbar>
