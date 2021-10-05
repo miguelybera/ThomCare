5010e584242c592ce22ebfae2bc992e6ebe2e0af
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-10-05 11:01:14 +0800

    updated link for form 6a

--- frontend/src/components/home/requests/forms/FormsList.js
+++ frontend/src/components/home/requests/forms/FormsList.js
@@ -30,7 +30,7 @@ const FormsList = () => {
                                         <i class="fa fa-edit-square-o" aria-hidden="true"></i> Fill out
                                     </Button>
                                 </Link>
-                                <Button variant="warning" href="https://drive.google.com/file/d/1T5FATdMcVEhhTsfrqkEOKudhWF7rkvE_/view?usp=sharing" target="_blank" rel="noreferrer" style={{ margin: '5px' }}>
+                                <Button variant="warning" href="https://drive.google.com/file/d/11UDJbETgsYGSlfwU6MH-fqQsaUFH5lMd/view?usp=sharing" target="_blank" rel="noreferrer" style={{ margin: '5px' }}>
                                     <i class="fa fa-eye" aria-hidden="true"></i> View
                                 </Button>
                             </td>
